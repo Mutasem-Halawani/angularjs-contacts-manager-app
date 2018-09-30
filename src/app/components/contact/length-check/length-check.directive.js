79fb03011830255013abff0bb2f45f902d517b82
mutasem <mutasem.halawani@gmail.com>
2018-09-30 18:09:17 +0300

    add length check directive for inputs

---
+++ src/app/components/contact/length-check/length-check.directive.js
@@ -0,0 +1,26 @@
+function lengthCheck () {
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		compile: function ($element) { // opposed to the link function this will add the class only once
+			$element.addClass('dynamic-input')
+			return function ($scope, $element, $attrs, $ctrl) {
+				// $ctrl will correspond the ngModel
+				var dynamicClass = 'dynamic-input--no-contents'
+				$scope.$watch(function () {
+					return $ctrl.$viewValue // return the current viewValue of the input it's bound to
+				}, function (newValue) {
+					if (newValue) {
+						$element.removeClass(dynamicClass)
+					} else {
+						$element.addClass(dynamicClass)
+					}
+				})
+			}
+ 		}
+	}
+}
+
+angular
+	.module('components.contact')
+	.directive('lengthCheck', lengthCheck)
